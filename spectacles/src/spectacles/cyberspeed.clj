175bc8b76c086eb118d7a47551a21039deb491ab
Aaron <tripnaces@gmail.com>
2016-02-25 07:23:53 -0500

    Adjustments to improve contrast in presentations

--- spectacles/src/spectacles/cyberspeed.clj
+++ spectacles/src/spectacles/cyberspeed.clj
@@ -5,16 +5,21 @@
 ;; Title: CyberSpeed
 ;; Author: Maria Jackson
 
-(defn setup [] {})
+(defn setup [] 
+;  (q/smooth)
+  (q/background 0 0 0)
+  {})
 
 (defn update [state] state)
 
 (defn draw [state]
   (q/fill 255 0 0)
+  (q/stroke-weight 2)
   (q/line 0 0 (q/random 0 1000)  (q/random 0 1000))
   (q/ellipse 50 50 100 100)
+  (q/stroke-weight 4)
   (q/stroke 255 (q/random 0 255) (q/random 0 255))
-  (q/fill 255 255 255 2)
+  (q/fill 0 0 0 2)
   (q/rect 0 0 (q/width) (q/height)))
 
 ;; (q/defsketch CyberSpeed
